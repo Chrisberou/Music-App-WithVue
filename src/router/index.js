e1bfa577dc917118312ac6de500f8e3042919699
Chrisberou <christosberou@gmail.com>
2023-10-13 11:01:40 +0300

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import useUserStore from '@/stores/User'
+
+const Home = () => import('@/views/Home.vue')
+const About = () => import('@/views/About.vue')
+const Manage = () => import('@/views/Manage.vue')
+const Song = () => import('@/views/Song.vue')
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: About
+  },
+  {
+    name: 'manage',
+    // alias: '/manage',
+    path: '/manage-music',
+    component: Manage,
+    //  beforeEnter: (to, from, naxt) => {}
+    meta: {
+      requiresAuth: true
+    }
+  },
+
+  {
+    path: '/manage',
+    redirect: { name: 'manage' }
+  },
+  {
+    name: 'song',
+    path: '/song/:id',
+    component: Song
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+})
+//checks for every visited path
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next()
+    return
+  }
+  const store = useUserStore()
+  if (store.userLoggedIn) {
+    next()
+  } else {
+    next({ name: 'home' })
+  }
+})
+
+export default router
